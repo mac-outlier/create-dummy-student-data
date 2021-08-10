bccc329bf05ae7a5c37f81a7fc7f7298afe23641
mac-outlier <mattc@outlier.org>
2021-08-10 11:38:11 -0400

    Added generation of json file grade data

--- index.js
+++ index.js
@@ -1,59 +1,100 @@
-
+/**
+ * @author Matt Curtis
+ * Generates fake student database data and corresponding test data in a json file.
+ */
 (async () => {
-  const sqlite = require('sqlite3').verbose()
+  const fs = require('fs')
   const faker = require('faker')
 
-  const RECORD_COUNT = 100000
+  const RECORD_COUNT = 10000
+  const COURSES = ['Calculus', 'Philosophy', 'Microeconomics', 'Astronomy', 'Statistics']
 
-  const db = new sqlite.Database('./students.sqlite3')
+  const db = require('knex')({
+    client: 'sqlite3',
+    connection: {
+      filename: './dist/students.db',
+    },
+    useNullAsDefault: true
+  })
+  const gradeDataFile = fs.createWriteStream('./dist/grades.json')
 
-  await db.exec(`DROP TABLE IF EXISTS students;
-    CREATE TABLE students (
-      first_name TEXT,
-      last_name TEXT,
-      email TEXT,
-      is_registered INTEGER,
-      is_approved INTEGER,
-      password_hash TEXT,
-      address TEXT,
-      city TEXT,
-      state TEXT,
-      zip TEXT,
-      phone TEXT,
-      created TEXT,
-      last_login TEXT,
-      ip_address TEXT
-    );`)
-
-  const stmt = await db.prepare('INSERT INTO students VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)')
+  await db.schema.dropTableIfExists('students')
+  const table = await db.schema.createTable('students', table => {
+    table.increments('id')
+    table.string('first_name')
+    table.string('last_name')
+    table.string('email')
+    table.integer('is_registered')
+    table.integer('is_approved')
+    table.string('password_hash')
+    table.string('address')
+    table.string('city')
+    table.string('state')
+    table.string('zip')
+    table.string('phone')
+    table.string('created')
+    table.string('last_login')
+    table.string('ip_address')
+  })
+  
+  await gradeDataFile.write('[\n')
 
   for (let i = 0; i < RECORD_COUNT; i++) {
 
-    const state = faker.address.stateAbbr()
-    const postalCode = faker.address.zipCodeByState(state)
-    const firstName = faker.name.firstName()
-    const lastName = faker.name.lastName()
+    const student = new Map()
+    student.set('first_name', faker.name.firstName())
+    student.set('last_name', faker.name.lastName())
+    student.set('email', faker.internet.email(student.get('first_name'), student.get('last_name')))
+    student.set('is_registered', faker.datatype.boolean() ? 1 : 0)
+    student.set('is_approved', faker.datatype.boolean() ? 1 : 0)
+    student.set('password_hash', faker.git.commitSha())
+    student.set('address', faker.address.streetAddress(true))
+    student.set('city', faker.address.cityName())
+    student.set('state', faker.address.stateAbbr())
+    student.set('zip', faker.address.zipCodeByState(student.get('state')))
+    student.set('phone', faker.phone.phoneNumber())
+    student.set('created', faker.date.recent())
+    student.set('last_login', faker.date.recent())
+    student.set('ip_address', faker.internet.ip())
 
-    stmt.run(
-      firstName,
-      lastName,
-      faker.internet.email(firstName, lastName),
-      faker.datatype.boolean() ? 1 : 0,
-      faker.datatype.boolean() ? 1 : 0,
-      faker.git.commitSha(),
-      faker.address.streetAddress(true),
-      faker.address.cityName(),
-      state,
-      postalCode,
-      faker.phone.phoneNumber(),
-      faker.date.recent(),
-      faker.date.recent(),
-      faker.internet.ip()
-    )
+    const res = await db('students').insert([Object.fromEntries(student)])
+    student.set('id', res[0])
+    const coursesRandom = getRandomElementsFromArray(COURSES)
+    const grades = []
+
+    for (const course of coursesRandom) {
+      const grade = Math.round(Math.random() * 100, 2)
+      grades.push(JSON.stringify({id: student.get('id'), course: course, grade: grade}))
+    }
+
+    await gradeDataFile.write(grades.join(',\n'))
+    if (i < RECORD_COUNT - 1 && coursesRandom.length) {
+      await gradeDataFile.write(',\n')
+    }
+    
   }
 
-  await stmt.finalize()
-
-  db.close()
-
+  await gradeDataFile.write('\n]')
+  gradeDataFile.close()
+  db.destroy()
 })()
+
+function getRandomElementsFromArray(arr) {
+  const numberOfRandomElementsToExtract = Math.floor(Math.random() * arr.length)
+  const elements = [];
+
+  function getRandomElement(arr) {
+    if (elements.length < numberOfRandomElementsToExtract) {
+      const index = Math.floor(Math.random() * arr.length)
+      const element = arr.splice(index, 1)[0];
+
+      elements.push(element)
+
+      return getRandomElement(arr)
+    } else {
+        return elements
+    }
+  }
+
+  return getRandomElement([...arr])
+}
